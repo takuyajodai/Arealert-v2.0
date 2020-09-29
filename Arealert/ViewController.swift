e57fb304cd4d23e830e261e60db3fe48744802ce
takuyajodai <j.takuya0625@icloud.com>
2020-09-29 09:55:54 +0900

    Initial Commit

---
+++ Arealert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arealert
+//
+//  Created by Jodai Takuya on 2020/09/29.
+//  Copyright © 2020 Jodai Takuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
